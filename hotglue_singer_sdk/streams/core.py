e3270aaf97a84e27772a7e385bb54e233cdb8b18
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2026-04-20 10:33:07 -0300

    feat(HGI-10095): implement `--get-available-filters` (#43)
    * feat: implement get-available-filters
    
    * test: implement tests for get-available-filters
    
    * chore: bump sdk version
    
    * fix: check returned record type
    
    * fix context parameter
    
    * fix: type annotation
    
    * fix: catalog presence checking

--- hotglue_singer_sdk/streams/core.py
+++ hotglue_singer_sdk/streams/core.py
@@ -21,6 +21,7 @@ from typing import (
     Type,
     TypeVar,
     Union,
+    Set,
     cast,
 )
 
@@ -164,6 +165,50 @@ class Stream(metaclass=abc.ABCMeta):
     def setup_selected_filters(self) -> None:
         """Setup selected filters for the stream."""
         pass
+
+    def get_available_filters_metadata(self) -> Dict[str, Any]:
+        """Describe available filters for this stream (for --get-available-filters).
+
+        Example:
+        {
+            "supported_operators": ["AND", "OR"],
+            "supports_nesting_clauses": True,
+            "filters": {
+                "filter_name": {
+                    "label": "Filter Name",
+                    "supported_operators": ["IN", "EQ"],
+                    "target_field": "field_name",
+                    "options": "reference_data.stream_name.field_name",
+                }
+            }
+        }
+
+        Override in taps that support user-configurable filters.
+        If no filters are supported, returns None.
+        """
+        pass
+
+    def get_available_filters_reference_data(self, fields_to_include: Set[str]) -> List[Dict[str, Any]]:
+        """Get reference data for for the stream.
+
+        Args:
+            fields_to_include: List of fields to get reference data for. This is the list of fields that are used in the "options" field of the filters.
+
+        Returns:
+            A list of dictionaries, each dictionary is a record from the stream.
+            The records fields are filtered to only include the fields in the `fields_to_include` list.
+        """
+        records = []
+        for record_result in self.get_records(context=None):
+            if isinstance(record_result, tuple):
+                # Tuple items should be the record and the child context
+                record, child_context = record_result
+            else:
+                record = record_result
+            filtered_record = {k: v for k, v in record.items() if k in fields_to_include}
+            if filtered_record:
+                records.append(filtered_record)
+        return records
 
     @property
     def stream_maps(self) -> List[StreamMap]:
